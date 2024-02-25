afe3eda9726dc1032fc236468701a670aadb2439
miiiingi <kimmingi722@gmail.com>
2024-02-25 13:16:26 +0900

    update

--- baekjoon/19238.py
+++ baekjoon/19238.py
@@ -1,7 +1,6 @@
 import sys
 from collections import deque
 
-
 def findDest(startX, startY, destX, destY, remainedFuel):
     # 손님을 태우고 목적지로 향하는 함수
     fuelMap = [[0 for _ in range(N)] for _ in range(N)]
@@ -10,8 +9,10 @@ def findDest(startX, startY, destX, destY, remainedFuel):
     locations.append([startX, startY, remainedFuel])
     while locations:
         x, y, F = locations.popleft()
+        # 연료가 0인 상태에서 한 칸 더 간 경우
         if F <= -1:
-            return -1
+            print(-1)
+            exit(0)
         if destX == x and destY == y:
             return F + (remainedFuel - F) * 2
         visited[x][y] = 1
@@ -22,10 +23,11 @@ def findDest(startX, startY, destX, destY, remainedFuel):
             uF = F - 1
             if ux < 0 or ux >= N or uy < 0 or uy >= N or visited[ux][uy] == 1 or wall[ux][uy] == 1:
                 continue
-            if visited[ux][uy] != 1 and wall[ux][uy] != 1:
-                visited[ux][uy] = 1
-                fuelMap[ux][uy] = uF
-                locations.append([ux, uy, uF])
+            visited[ux][uy] = 1
+            fuelMap[ux][uy] = uF
+            locations.append([ux, uy, uF])
+
+    # 손님을 찾지 못했는데, 더 이상 갈 수 있는 곳이 없는 상태 -> 갇혔다거나.. 
     print(-1)
     exit(0)
 
@@ -38,6 +40,7 @@ def bfs(startX, startY, fuel):
     finded = False
     findedList = []
     remainedFuel = fuel
+    # 만약 택시의 시작점에 손님이 있는 경우 -> 바로 목적지 탐색.
     if startList[startX][startY] != 0:
         finededNumber = startList[startX][startY]
         destX, destY = endList[finededNumber-1]
@@ -47,24 +50,27 @@ def bfs(startX, startY, fuel):
     while locations:
         x, y, F = locations.popleft()
         if F <= -1:
-            return -1, -1, -1
+            # 연료가 0인 상태에서 한 칸 더 간 경우
+            print(-1)
+            exit(0)
         visited[x][y] = 1
         fuelMap[x][y] = F
-        # 이렇게 가야 행 > 열 순서로 탐색을하고 행 번호가 커지지않도록 탐색가능하다.
+        # 행 > 열 순서로 탐색, 행 번호가 커지지않도록 탐색.
         for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
             ux = x + dx
             uy = y + dy
             uF = F - 1
             if ux < 0 or ux >= N or uy < 0 or uy >= N or visited[ux][uy] == 1 or wall[ux][uy] == 1:
                 continue
-            if visited[ux][uy] != 1 and wall[ux][uy] != 1:
-                visited[ux][uy] = 1
-                fuelMap[ux][uy] = uF
-                locations.append([ux, uy, uF])
+            visited[ux][uy] = 1
+            fuelMap[ux][uy] = uF
+            locations.append([ux, uy, uF])
 
+        # 같은 거리에 손님이 있는 경우, 그 손님을 findedList에 추가함. -> bfs이기 때문에 처리를 해줘야함.
         if startList[x][y] != 0 and finded and remainedFuel == F:
             findedList.append([x, y])
 
+        # 찾은 상태에서 더 탐색하려고 할 때, 바로 찾은 손님들 중에서 가장 행 번호, 열 번호가 작은 손님의 목적지를 향해서 출발함.
         if F < remainedFuel and finded:
             findedList.sort()
             startX, startY = findedList[0][0], findedList[0][1]
@@ -74,6 +80,7 @@ def bfs(startX, startY, fuel):
             startList[startX][startY] = 0
             return destX, destY, regainedFuel
 
+        # 처음 손님을 만났을 때, finded 함수를 True로 설정
         if startList[x][y] != 0 and not finded:
             finded = True
             remainedFuel = F
@@ -83,10 +90,12 @@ def bfs(startX, startY, fuel):
                 startX, startY = findedList[0][0], findedList[0][1]
                 finededNumber = startList[startX][startY]
                 destX, destY = endList[finededNumber-1]
-                regainedFuel = findDest(startX, startY, destX, destY, remainedFuel)
+                regainedFuel = findDest(
+                    startX, startY, destX, destY, remainedFuel)
                 startList[startX][startY] = 0
                 return destX, destY, regainedFuel
 
+    # 손님을 찾지 못했는데, 더 이상 갈 수 있는 곳이 없는 상태 -> 갇혔다거나.. 
     print(-1)
     exit(0)
 
