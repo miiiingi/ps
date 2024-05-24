f4224beae126f4ad2548512dba3f4ca8f0d59194
miiiingi <kimmingi722@gmail.com>
2024-05-24 14:57:22 +0900

    brute force

--- baekjoon/1107.py
+++ baekjoon/1107.py
@@ -1,3 +1,5 @@
+# 하나씩 숫자를 비교하면서 만들어가려고 했는데, 반복문 안의 숫자를 비교해서 끝내면 된다!!
+# 역시 효율적인 문제 접근 방식을 더 생각해봐야할 듯! 먼저 구현하려고 하지 말고!
 import sys
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
@@ -5,21 +7,17 @@ if M > 0:
     deleted = list(map(int, sys.stdin.readline().split()))
 else:
     deleted = []
+min_num = abs(100 - N)
 if N == 100:
     print(0)
 else:
-    numbers = list(set([num for num in range(0, 10)]) - set(deleted))
-    answer = ''
-    for num in list(str(N)):
-        min_number = 10
-        min_index = None
-        if int(num) == 0 and min_number > 0:
-            num = 10
+    for num in range(1000001):
+        str_num = str(num)
+        for digit in str_num:
+            if int(digit) in deleted:
+                break
         else:
-            num = int(num)
-        for i in range(len(numbers)):
-            if abs(int(num) - numbers[i]) < min_number:
-                min_number = int(num) - numbers[i]
-                min_index = i
-        answer += str(numbers[min_index])
-    print(len(answer) + abs(N - int(answer)))
+            # 모든 숫자를 비교하지 말고, 마지막 끝자리 숫자인 경우만 min 값을 계산하고 비교하여 업데이트 하자!
+            if digit == str_num[-1]:
+                min_num = min(min_num, abs(num - N) + len(str_num))
+    print(min_num)
