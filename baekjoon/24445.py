df86c5e85b0a9cc9a9b8b88e03eba6d3f7040d25
miiiingi <kimmingi722@gmail.com>
2024-06-25 17:07:34 +0900

    bfs

---
+++ baekjoon/24445.py
@@ -0,0 +1,28 @@
+import sys
+import collections
+
+def bfs(startV):
+    global visited_order
+    queue = collections.deque([startV])
+    visited[startV] = visited_order
+    while queue:
+        q = queue.popleft()
+        for v in graph[q]:
+            if visited[v] == 0:
+                queue.append(v)
+                visited_order += 1
+                visited[v] = visited_order
+
+N, M, R = map(int, sys.stdin.readline().split())
+graph = collections.defaultdict(list)
+visited = [0] * (N+1)
+visited_order = 1
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for k in graph.keys():
+    graph[k] = sorted(graph[k], reverse = True)
+bfs(R)
+for ind in range(1, N+1):
+    print(visited[ind])
