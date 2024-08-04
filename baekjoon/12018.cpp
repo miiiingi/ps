4b49838c80fbc816c9d30819265ab8e805945f47
miiiingi <kimmingi722@gmail.com>
2024-08-04 15:51:58 +0900

    배열

--- baekjoon/12018.cpp
+++ baekjoon/12018.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int numberOfSubject, mileage, p, l;
+    int numberOfSubject, mileage, p, l, count = 0;
     cin >> numberOfSubject >> mileage;
     vector<int> pList;
+    vector<int> answer;
 
     for (int i = 0; i < numberOfSubject; i++)
     {
@@ -15,13 +17,25 @@ int main() {
         {
             cin >> pList[i];
         }
-        for (int i = 0; i < p; i++)
+        if (p < l)
         {
-            cout << pList[i] << endl;
+            answer.push_back(1);
+        } else
+        {
+            sort(pList.begin(), pList.end());
+            answer.push_back(pList[p-l]);
         }
-        
     }
-
-    
+    sort(answer.begin(), answer.end());
+	int cnt = 0;
+	for (int i : answer)
+	{
+		if (mileage >= i)
+		{
+			count++;
+			mileage -= i;
+		}
+	}
+    cout << count;
     return 0;
 }
