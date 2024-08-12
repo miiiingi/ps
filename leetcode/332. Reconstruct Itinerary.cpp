4f532e388af0b87e8a033fa77170b32f771af206
miiiingi <kimmingi722@gmail.com>
2024-08-12 23:45:00 +0900

    dfs

---
+++ leetcode/332. Reconstruct Itinerary.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+vector<string> result;
+
+class Solution {
+    /*
+    해결해야하는 문제
+    1. value의 정렬 문제 (ATL < SFO)
+    2. 재참조의 문제 (JFK  -> ATL -> JFK)
+    */
+public:
+    unordered_map<string, map<string, bool>> newTickets;
+    vector<string> answer;
+
+    void dfs(string path){
+        map<string, bool>& value = newTickets[path];
+        if (value.size() == 0)
+        {
+            return;
+        }
+        answer.push_back(path);
+        for (auto v: value)
+        {
+            if (v.second)
+            {
+                continue;
+            }
+            newTickets[path].erase(v.first);
+            dfs(v.first);
+            newTickets[path][v.first] = false;
+        }
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for (auto i = 0; i < tickets.size(); i++)
+        {
+            string key = tickets[i][0];
+            newTickets[key][tickets[i][1]] = false;
+        }
+        dfs("JFK");
+        return answer;
+    }
+};
+
+int main(){
+    Solution sol;
+    // vector<vector<string>> tickets = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
+    // vector<vector<string>> tickets = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
+    vector<vector<string>> tickets = {{"JFK","KUL"},{"JFK","NRT"},{"NRT","JFK"}};
+    result = sol.findItinerary(tickets);
+    return 0;
+}
