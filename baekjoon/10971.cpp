3945ab014ef9f334ea4d4e89c84bcac7b475b0b7
miiiingi <kimmingi722@gmail.com>
2024-09-03 00:18:09 +0900

    dfs + 백트래킹 기본 문제

--- baekjoon/10971.cpp
+++ baekjoon/10971.cpp
@@ -1,32 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
-int N;
+int N, minValue = 10000001;
 #define MAX 10
 bool visited[MAX];
 int W[MAX][MAX];
 
-void dfs(int startP)
+void dfs(int startP, int curP, int cnt, int sum)
 {
+    if (cnt == N)
+    {
+        if (W[curP][startP] == 0)
+        {
+            return;
+        }
+        if (minValue > sum + W[curP][startP])
+        {
+            minValue = sum + W[curP][startP];
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (visited[i] || W[curP][i] == 0)
+        {
+            continue;
+        }
+        visited[i] = true;
+        dfs(startP, i, cnt + 1, sum + W[curP][i]);
+        visited[i] = false;
+    }
 }
 
 int main()
 {
     cin >> N;
     memset(W, 0, sizeof(W));
-    for (size_t i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (size_t j = 0; j < N; j++)
+        for (int j = 0; j < N; j++)
         {
             cin >> W[i][j];
         }
     }
-    for (size_t i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        memset(visited, false, sizeof(visited));
-        dfs(i+1);
+        visited[i] = true;
+        dfs(i, i, 1, 0);
+        visited[i] = false;
     }
+    cout << minValue;
 
     return 0;
 }
