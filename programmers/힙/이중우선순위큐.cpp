c35ed1262a6c6249b1fa47d8aec37327e56ec607
miiiingi <kimmingi722@gmail.com>
2024-09-06 22:47:57 +0900

    이중우선순위큐-multiset

--- programmers/힙/이중우선순위큐.cpp
+++ programmers/힙/이중우선순위큐.cpp
@@ -1,20 +1,16 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
-
+#include <set>
 using namespace std;
-
 vector<int> solution(vector<string> operations)
 {
-    priority_queue<int> maxPQ;
-    priority_queue<int, std::vector<int>, std::greater<int>> minPQ;
     vector<int> answer;
     istringstream iss;
     string str;
     int number;
-    int insertCnt = 0;
+    multiset<int> ms;
 
     for (size_t i = 0; i < operations.size(); i++)
     {
@@ -22,62 +18,32 @@ vector<int> solution(vector<string> operations)
         iss >> str >> number;
         if (str == "I")
         {
-            maxPQ.push(number);
-            minPQ.push(number);
-            insertCnt++;
+            ms.insert(number);
         }
         else if (str == "D")
         {
-            if (number == -1)
+            if (!ms.empty())
             {
-                if (!minPQ.empty())
+                if (number == 1)
                 {
-                    minPQ.pop();
-                    insertCnt--;
+                    ms.erase(--ms.end());
                 }
-            }
-            else
-            {
-                if (!maxPQ.empty())
+                else if (number == -1)
                 {
-                    maxPQ.pop();
-                    insertCnt--;
+                    ms.erase(ms.begin());
                 }
-            }
-        }
-        if (insertCnt == 0)
-        {
-            while (!maxPQ.empty())
-            {
-                maxPQ.pop();
-            }
-            while (!minPQ.empty())
-            {
-                minPQ.pop();
             }
         }
     }
-    if (insertCnt == 0)
+    if (ms.empty())
     {
         answer.push_back(0);
         answer.push_back(0);
     }
     else
     {
-        answer.push_back(maxPQ.top());
-        answer.push_back(minPQ.top());
+        answer.push_back(*--ms.end());
+        answer.push_back(*ms.begin());
     }
-
     return answer;
-}
-
-int main()
-{
-    vector<int> answer2 = solution({"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"});
-    for (size_t i = 0; i < answer2.size(); i++)
-    {
-        cout << answer2[i];
-    }
-
-    return 0;
 }
