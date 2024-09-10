3250dbabe86892c3b75d219fd6f4d9c0a0d4da1f
miiiingi <kimmingi722@gmail.com>
2024-09-10 22:11:55 +0900

    스택,큐..?

---
+++ programmers/2019 카카오 개발자 겨울 인턴십/징검다리건너기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> stones, int k) {
+    int answer = 0;
+    return answer;
+}
+
+int main(){
+    vector<int> stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+    int k = 3;
+    int answer = solution(stones, k);
+    return 0;
+}
