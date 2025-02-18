c8c5c0cc6db09e2f78d7cf25772480b45ae2a1cb
miiiingi <kimmingi722@gmail.com>
2025-02-18 12:06:24 +0900

    투 포인터 문제

--- baekjoon/2003.cpp
+++ baekjoon/2003.cpp
@@ -17,24 +17,17 @@ int main()
     long long sum = 0, cnt = 0;
     while (true)
     {
-        // cout << "=====================" << "\n";
-        // cout << "pre sum : " << sum << "\n";
-        // cout << "pre start: " << start << " pre end: " << end << "\n";
         if (sum >= M)
         {
             if (sum == M)
             {
                 cnt++;
-                // cout << "cnt: " << cnt << "\n";
             }
             sum -= arr[start++];
             if (start > N)
             {
                 break;
             }
-
-            // cout << "match post sum : " << sum << "\n";
-            // cout << "match post start: " << start << " post end: " << end << "\n";
         }
         else
         {
@@ -43,9 +36,6 @@ int main()
             {
                 break;
             }
-
-            // cout << "post sum : " << sum << "\n";
-            // cout << "post start: " << start << " post end: " << end << "\n";
         }
     }
     cout << cnt;
