648bfdc8736488705d954b30c9bad508c5f8a171
miiiingi <kimmingi722@gmail.com>
2026-04-23 00:27:13 +0900

    풀이완료

--- baekjoon/15686.py
+++ baekjoon/15686.py
@@ -16,21 +16,20 @@ for r in range(N):
             chicken.append([r, i])
     maps.append(row)
 candidate = list(combinations(chicken, M))
-print(house)
-print(candidate)
 # 3개를 고랐는데 어떻게 각 치킨집별로 최소 거리를 구할 것인가 ? > 모든 위치에서 각 치킨집까지의 거리를 구하고 최소 값을 도출한다.
 cnt = []
+all_chicken_dist = 10e9
 for c1 in candidate:
     accum = 0
-    for c2 in c1:
-        yc, xc = c2
-        dist = []
-        for h in house:
-            y, x = h
-            dist.append(abs(yc - y) + abs(xc - x))
-            print(f"h: {h}, yc: {yc}, xc: {xc}, dist: {abs(yc-y) + abs(xc-x)}")
-        accum += sum(dist)
-    cnt.append(accum)
-print(F'cnt: {cnt}')
-print(f"cnt min ind: {cnt.index(min(cnt))}")
-print(f'f candi: {candidate[9]}')
+    for h in house:
+        y, x = h
+        min_chicken_dist = 10e9
+        for c2 in c1:
+            yc, xc = c2
+            dist = abs(yc - y) + abs(xc - x)
+            if dist < min_chicken_dist:
+                min_chicken_dist = dist
+        accum += min_chicken_dist
+    if accum < all_chicken_dist:
+        all_chicken_dist = accum
+print(all_chicken_dist)
