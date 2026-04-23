90f8c7ecea38e4f90cca0bc8cf0796ac8bb5a4a7
miiiingi <kimmingi722@gmail.com>
2026-04-23 23:09:57 +0900

    구현 문제 보완

--- baekjoon/21608.py
+++ baekjoon/21608.py
@@ -12,77 +12,47 @@ for _ in range(N * N):
     student[poped] = temp
 room = [[0 for _ in range(N)] for _ in range(N)]
 registered = []
-for ind, (k, v) in enumerate(student.items()):
-    if ind == 0:
-        room[N // 2][N // 2] = k
-        registered.append(k)
-        print(room)
-    else:
-        liked = []
-        for vi in v:
-            if vi in registered:
-                liked.append(vi)
-        target = []
-        print(f"ind: {ind}, liked: {liked}")
-        for i in range(N):
-            for j in range(N):
-                if liked:
-                    for l in liked:
-                        if room[i][j] == l:
-                            for m in MOVE:
-                                x, y = m
-                                nx, ny = i + x, j + y
-                                if (
-                                    nx < 0
-                                    or nx >= N
-                                    or ny < 0
-                                    or ny >= N
-                                    or room[nx][ny] != 0
-                                ):
-                                    continue
-                                cnt = 0
-                                for m2 in MOVE:
-                                    x2, y2 = m2
-                                    nx2, ny2 = nx + x2, ny + y2
-                                    if (
-                                        nx2 < 0
-                                        or nx2 >= N
-                                        or ny2 < 0
-                                        or ny2 >= N
-                                        or room[nx2][ny2] != 0
-                                    ):
-                                        continue
-                                    print(f"a nx2: {nx2}, ny2: {ny2}")
-                                    cnt += 1
-                                target.append([nx, ny, cnt])
-                                print(f"58 target: {target}")
-                else:
-                    if room[i][j] != 0:
-                        continue
-                    for m in MOVE:
-                        x, y = m
-                        nx, ny = i + x, j + y
-                        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-                            continue
-                        cnt = 0
-                        for m2 in MOVE:
-                            x2, y2 = m2
-                            nx2, ny2 = nx + x2, ny + y2
-                            if (
-                                nx2 < 0
-                                or nx2 >= N
-                                or ny2 < 0
-                                or ny2 >= N
-                                or room[nx2][ny2] != 0
-                            ):
-                                continue
-                            cnt += 1
-                        target.append([nx, ny, cnt])
-        if target:
-            target.sort(key=lambda x: (-x[2], x[0], x[1]))
-            print(f'83 target: {target}')
-            x, y, c = target[0]
-            room[x][y] = k
-            registered.append(k)
-            print(room)
-# print(room)
+for k, v in student.items():
+    seat_candidate = []
+    for i in range(N):
+        for j in range(N):
+            if room[i][j] != 0:
+                continue
+            liked_friend = 0
+            blank_seat = 0
+            for m in MOVE:
+                x, y = m
+                nx, ny = i + x, j + y
+                if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                    continue
+                if room[nx][ny] == 0:
+                    blank_seat += 1
+                if room[nx][ny] in v:
+                    liked_friend += 1
+            seat_candidate.append([liked_friend, blank_seat, i, j])
+    seat_candidate.sort(key=lambda x: (-x[0], -x[1], x[2], x[3]))
+    best_candidate = seat_candidate[0]
+    room[best_candidate[2]][best_candidate[3]] = k
+
+room_satisfaction = 0
+for i in range(N):
+    for j in range(N):
+        student_satisfaction = 0
+        for m in MOVE:
+            x, y = m
+            nx, ny = i + x, j + y
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                continue
+            if room[nx][ny] in student[room[i][j]]:
+                student_satisfaction += 1
+        if student_satisfaction == 0:
+            room_satisfaction += 0
+        elif student_satisfaction == 1:
+            room_satisfaction += 1
+        elif student_satisfaction == 2:
+            room_satisfaction += 10
+        elif student_satisfaction == 3:
+            room_satisfaction += 100
+        elif student_satisfaction == 4:
+            room_satisfaction += 1000
+print(room_satisfaction)
